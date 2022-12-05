51f96b46658aee96b3ff33a57c5997d292058e61
TuralJSDeveloper <tural.azizxanli92@gmail.com>
2022-12-05 21:45:27 +0400

    Java massiv çap etmə

---
+++ JavaArr/src/javaarr/JavaArr.java
@@ -0,0 +1,16 @@
+package javaarr;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Tural
+ */
+public class JavaArr {
+
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5};
+        System.out.println("Massiv: "+Arrays.toString(a));
+    }
+    
+}
